f9fb43f1e580e2c0e23799013ad5fbd30cf234ad
Kong To <newlight77@gmail.com>
2019-04-30 23:03:18 +0530

    account deposit feature : all 2 scenarios passed

--- src/main/java/com/newlight77/kata/bank/service/OperationService.java
+++ src/main/java/com/newlight77/kata/bank/service/OperationService.java
@@ -1,5 +1,6 @@
 package com.newlight77.kata.bank.service;
 
+import com.newlight77.kata.bank.exception.NotAllowedException;
 import com.newlight77.kata.bank.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,10 @@ public class OperationService {
     private AccountService accountService;
 
     public double deposit(String accountId, Double money) {
+        if (money < 0) {
+            throw new NotAllowedException("not allowed");
+        }
+
         Account account = accountService.getAccount(accountId);
         double balance = account.getBalance() + money;
         account.setBalance(balance);
