dd15034ab0296772919ede5ce796f0fb46a6c8ca
Kong To <newlight77@gmail.com>
2019-11-03 22:53:44 +0100

    using masterthought/maven-cucumber-reporting and spacialcircumstances.gradle-cucumber-reporting to generate cucumber reports

--- src/test/java/cucumber/RunCucumberTest.java
+++ src/test/java/cucumber/RunCucumberTest.java
@@ -10,8 +10,6 @@ import org.junit.runner.RunWith;
             "pretty",
             "json:target/cucumber/cucumber.json",
             "json:build/cucumber/cucumber.json",
-            "de.monochromata.cucumber.report.PrettyReports:build/cucumber/monochromata",
-            "de.monochromata.cucumber.report.PrettyReports:target/cucumber/monochromata"
     },
     features = {
             "features"
